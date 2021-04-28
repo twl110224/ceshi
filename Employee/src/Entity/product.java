4d4bf3aef8a7a90fc20c5a84b35cf764c46fa24b
twl_glb <goodMorning_pro@at.bdqn.com>
2021-04-28 10:51:32 +0800

    1.01

---
+++ Employee/src/Entity/product.java
@@ -0,0 +1,18 @@
+package Entity;
+
+public class product {
+		private int id;
+		public int getId() {
+			return id;
+		}
+		public void setId(int id) {
+			this.id = id;
+		}
+		public String getPtname() {
+			return ptname;
+		}
+		public void setPtname(String ptname) {
+			this.ptname = ptname;
+		}
+		private String ptname;
+}
